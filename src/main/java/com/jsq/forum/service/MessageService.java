71c344fe3e16224e13d64402d5c0c01ee826a9cb
Your Name <949038401@qq.com>
2021-04-08 01:38:06 +0800

    update

--- src/main/java/com/jsq/forum/service/MessageService.java
+++ src/main/java/com/jsq/forum/service/MessageService.java
@@ -1,7 +1,15 @@
 package com.jsq.forum.service;
 
+import com.jsq.forum.dao.MessageDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MessageService {
+    @Autowired
+    MessageDao messageDao;
+
+    public void addMessage(){
+
+    }
 }
